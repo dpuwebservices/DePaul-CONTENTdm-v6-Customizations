abf8da8801dad6ab25a61c4049eb9315686a5357
Jim LeFager <jameslefager@gmail.com>
2013-02-14 11:21:24 -0600

    Added Menu files

--- scripts/googleanalytics.js
+++ scripts/googleanalytics.js
@@ -1,21 +1,26 @@
 // JavaScript Document
 //Google Analytics
-
-
+//Set tracking to base report
   var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-5169126-1']);
-  _gaq.push(['_trackPageview']);
+  _gaq.push(['_setAccount', 'UA-XXXXXX-X']);
+//Set Custom Variable to Platform-CONTENTdm
+_gaq.push(['_setCustomVar',
+      1,                   // This custom var is set to slot #1.  Required parameter.
+      'platform',           // The top-level name for our report.  Required parameter.
+      'contentdm',  // Sets the value of "Platform".  Required parameter.
+      3                    // Sets the scope to page-level.  Optional parameter.
+   ]);
 
-  (function() {
+_gaq.push(['_trackPageview']);
+
+(function() {
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
 
-
 try{
-var pageTracker = _gat._getTracker("UA-5169126-1");
-pageTracker._setDomainName("none");
+var pageTracker = _gat._getTracker("UA-XXXXXX-X");
 pageTracker._setAllowLinker(true);
 pageTracker._trackPageview();
 } catch(err) {}
