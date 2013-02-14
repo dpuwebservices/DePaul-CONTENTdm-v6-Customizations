44f116a765e9f18be293e1179bbd0ac126f85eb2
Jim LeFager <jameslefager@gmail.com>
2013-02-14 10:42:49 -0600

    Original File Copy
    Copy of files from server

---
+++ scripts/googleanalytics.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+//Google Analytics
+
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-5169126-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+
+try{
+var pageTracker = _gat._getTracker("UA-5169126-1");
+pageTracker._setDomainName("none");
+pageTracker._setAllowLinker(true);
+pageTracker._trackPageview();
+} catch(err) {}
